e655bf5aea3aeb2edaea0ef870dd3c207241daba
joel.harris <joelharris@zapatatechnology.com>
2018-05-03 20:42:38 -0400

    init

---
+++ src/antscheduler/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package antscheduler.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
---
+++ src/antscheduler/jpacontrollers/ReminderJpaController.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antscheduler.jpacontrollers;
+
+import antscheduler.exceptions.NonexistentEntityException;
+import antscheduler.exceptions.PreexistingEntityException;
+import antscheduler.model.Reminder;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author JoelHarris
+ */
+public class ReminderJpaController implements Serializable {
+
+    public ReminderJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Reminder reminder) throws PreexistingEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(reminder);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            if (findReminder(reminder.getReminderId()) != null) {
+                throw new PreexistingEntityException("Reminder " + reminder + " already exists.", ex);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Reminder reminder) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            reminder = em.merge(reminder);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = reminder.getReminderId();
+                if (findReminder(id) == null) {
+                    throw new NonexistentEntityException("The reminder with id " + id +
+                            " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Integer id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Reminder reminder;
+            try {
+                reminder = em.getReference(Reminder.class, id);
+                reminder.getReminderId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The reminder with id " + id +
+                        " no longer exists.", enfe);
+            }
+            em.remove(reminder);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Reminder> findReminderEntities() {
+        return findReminderEntities(true, -1, -1);
+    }
+
+    public List<Reminder> findReminderEntities(int maxResults, int firstResult) {
+        return findReminderEntities(false, maxResults, firstResult);
+    }
+
+    private List<Reminder> findReminderEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Reminder.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Reminder findReminder(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Reminder.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getReminderCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Reminder> rt = cq.from(Reminder.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
